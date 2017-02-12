30a652fbc8e75b82d2333387515619a7820896c5
Frank Ruben <FrankRuben27@gmail.com>
2017-02-12 08:57:16 +0100

    Remove final BYE

--- dp296_easy_twelve_days.fs
+++ dp296_easy_twelve_days.fs
@@ -56,8 +56,5 @@
 
 day-loop
 
-bye
-
-\ See https://www.reddit.com/r/dailyprogrammer/comments/5j6ggm/20161219_challenge_296_easy_the_twelve_days_of/
-
+\ See: https://www.reddit.com/r/dailyprogrammer/comments/5j6ggm/20161219_challenge_296_easy_the_twelve_days_of/
 \ Run with: gforth dp296easy.fs
